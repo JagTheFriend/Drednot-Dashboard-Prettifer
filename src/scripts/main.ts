6623a964d3de944fde6d708d4d7cc7896630b1fd
JagTheFriend <jagthefriend12@gmail.com>
2021-10-16 16:17:19 +0200

    Created a basic website which does the task

---
+++ src/scripts/main.ts
@@ -0,0 +1,37 @@
+const API_URL: string = "https://drednot.io/api/";
+const tableBody = <HTMLTableElement> document.getElementById("tableBody");
+
+interface Ship {
+    ship_name: string
+    score: number,
+    color: number,
+    hex_code: string,
+}
+
+interface APIResponse {
+    ships: Ship[]
+}
+
+let value: Ship;
+function getShips() {
+    fetch(`${API_URL}/scoreboard`)
+        .then((data: Response) => data.json())
+        .then((data: APIResponse) => data.ships)
+        .then(function (data: Ship[]) {
+            // return data
+            for (value of data) {
+                console.log(value);
+                const ship_name: string = value.ship_name;
+                const score: string = value.score.toString();
+                const color: string = value.color.toString();
+                const hex_code: string = value.hex_code;
+
+                var row = tableBody.insertRow(-1);
+
+                for (let val of [ship_name, score, color, hex_code]){
+                    var cell = row.insertCell(-1);
+                    cell.innerText = val;
+                }
+            }
+        })
+}
